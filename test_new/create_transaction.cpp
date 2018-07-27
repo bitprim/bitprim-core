d16cf69a6c769f2a2aa6913e4f3710cc39c4dc40
Ramiro Carlucho <ramirocarlucho@gmail.com>
2018-07-27 12:20:13 -0300

    [KEOK-13] unit tests fix

--- test_new/create_transaction.cpp
+++ test_new/create_transaction.cpp
@@ -55,9 +55,9 @@ TEST_CASE("[keoken__send_token__expected]") {
     outputs_to_spend.push_back({hash_to_spend, index_to_spend});
     bitprim::keoken::asset_id_t asset_id = 1;
     bitprim::keoken::amount_t amount_tokens = 1;
-    auto result = tx_encode_send_token(outputs_to_spend, bc::wallet::payment_address(WALLET), 21647102398, bc::wallet::payment_address(WALLETDESTINY), 20000, asset_id, amount_tokens);
-    // BOOST_REQUIRE_EQUAL(1, 1);
-    REQUIRE(true); 
+    auto result_tx = tx_encode_send_token(outputs_to_spend, bc::wallet::payment_address(WALLET), 21647102398, bc::wallet::payment_address(WALLETDESTINY), 20000, asset_id, amount_tokens);
+    std::string result = "01000000019373b022dfb99400ee40b8987586aea9e158f3b0c62343d59896c212cee60d980100000000ffffffff03204e0000000000001976a9147fa36605e302ed00aeca0da8e2743772df11290188acce3b440a050000001976a914b43ff4532569a00bcab4ce60f87cdeebf985b69a88ac0000000000000000176a0400004b50100000000100000001000000000000000100000000";
+    REQUIRE(result == libbitcoin::encode_base16(result_tx.second.to_data())); 
 }
 
 TEST_CASE("[keoken__create_asset__expected]") {
@@ -68,9 +68,10 @@ TEST_CASE("[keoken__create_asset__expected]") {
     outputs_to_spend.push_back({hash_to_spend, index_to_spend});
     std::string name = "testcoin";
     bitprim::keoken::amount_t amount_tokens = 1;
-    auto result = tx_encode_create_asset(outputs_to_spend, bc::wallet::payment_address(WALLET), 21647102398, name, amount_tokens);
-    // BOOST_REQUIRE_EQUAL(1, 1);
-    REQUIRE(true); 
+    auto result_tx = tx_encode_create_asset(outputs_to_spend, bc::wallet::payment_address(WALLET), 21647102398, name, amount_tokens);
+   std::string result = "01000000019373b022dfb99400ee40b8987586aea9e158f3b0c62343d59896c212cee60d980100000000ffffffff02ee89440a050000001976a914b43ff4532569a00bcab4ce60f87cdeebf985b69a88ac00000000000000001c6a0400004b50150000000074657374636f696e00000000000000000100000000";
+
+    REQUIRE(result == libbitcoin::encode_base16(result_tx.second.to_data())); 
 }
 
 
